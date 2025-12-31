50b7454571a4f09e59422fc03403d11d86946b97
SerAnatolii <anatoliy.serduikov@gmail.com>
2025-12-31 16:35:38 +0100

    BE deployed

--- README.md
+++ README.md
@@ -14,3 +14,13 @@ Fintech app for autónomos in Spain: Automate IVA/IRPF reports with AI.
 - Auth: Keycloak
 
 License: MIT
+
+## Deploy BE
+1. Build for linux/amd64 platform
+docker build --platform linux/amd64 -t taxhelper-backend .
+
+2. Tag it
+docker tag taxhelper-backend:latest 471754640546.dkr.ecr.eu-north-1.amazonaws.com/taxhelper-backend:latest
+
+3.  Push it
+docker push 471754640546.dkr.ecr.eu-north-1.amazonaws.com/taxhelper-backend:latest
--- backend/database.py
+++ backend/database.py
@@ -5,7 +5,13 @@ from sqlalchemy.dialects.postgresql import BYTEA
 import os
 from datetime import datetime
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:password@localhost:5432/taxhelper"
+DATABASE_HOST = os.getenv("DATABASE_HOST", "localhost")
+DATABASE_USERNAME = os.getenv("DATABASE_USERNAME", "postgres")
+DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD", "password")
+DATABASE_NAME = os.getenv("DATABASE_NAME", "taxhelper")
+DATABASE_PORT = os.getenv("DATABASE_PORT", "5432")
+
+SQLALCHEMY_DATABASE_URL = f"postgresql://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
